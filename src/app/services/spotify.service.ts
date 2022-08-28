66f4594a4ac92885532df68edf9a99e9087703b2
yisus1002 <yisusgarcia1002@gmail.com>
2022-08-28 17:25:16 -0500

    Se agrego un metodo para obtener el token

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,23 +1,57 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 //filtrar informacion
-import { map } from 'rxjs/operators';
+import { finalize, map } from 'rxjs/operators'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
+  token:any='';
 
   constructor(private http:HttpClient) { 
     console.log('servicio listo')
+    this.getToken();
+    setInterval(()=>{
+      this.getToken();
+    },3600000)
+    console.log(this.token)
+  }
+  postAccesToken():Observable<any>{
+    const payload = new HttpParams()
+  .set('grant_type', 'client_credentials')
+  .set('client_id', '009d42a7c8834de8b5777d6acbd9012b')
+  .set('client_secret', '7711aa9d5a7341749bead459d4538e12');
+  return this.http.post<any>(`https://accounts.spotify.com/api/token`, payload).pipe(map((resp: any) => resp));
+  }
+   getToken(){
+   this.postAccesToken()
+    .pipe(finalize(()=>{
+      const localS=(localStorage.getItem('token'));
+      localStorage.setItem('token', JSON.stringify(this.token))
+      if(localS){
+        this.token=JSON.parse(localS!)
+      }
+      // this.token=JSON.parse(localS!)
+    }))
+    .subscribe({
+      next: (data:any)=>{
+        this.token=(data.access_token)
+      },
+      error:(error:any)=>{
+        console.log(error)
+      },
+    })
   }
 
   getquery(query:String){
+    const token= (localStorage.getItem('token'));
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers =new HttpHeaders({
-      'Authorization': 'Bearer BQBz3rsm6YTQBFkYOCy48ZUbXr_xClGD8NMl_QnJhpSI2k2d-iyVhW8KsIWkogTpwEOSEkfLOHz-twXIRhSU2bvziLGkLkUr1tQI353Xz_Dtxe3QQ4c'
+      'Authorization': `Bearer ${JSON.parse(token!)}` 
     }); 
     return this.http.get(url, {headers})
   } 
